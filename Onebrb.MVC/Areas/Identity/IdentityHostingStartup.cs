b0f8b6a625f5b0600a7e96cf80b31096c3d15cb2
kaldren <drenski666@gmail.com>
2020-05-22 22:18:32 +0300

    Switched IdentityUser with ApplicationUser

---
+++ Onebrb.MVC/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Onebrb.MVC.Data;
+using Onebrb.MVC.Models;
+
+[assembly: HostingStartup(typeof(Onebrb.MVC.Areas.Identity.IdentityHostingStartup))]
+namespace Onebrb.MVC.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
