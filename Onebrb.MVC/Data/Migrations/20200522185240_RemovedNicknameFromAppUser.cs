6f72b1233af63070e7428737897291f93bd76525
kaldren <drenski666@gmail.com>
2020-05-22 21:53:11 +0300

    Removed Nickname

---
+++ Onebrb.MVC/Data/Migrations/20200522185240_RemovedNicknameFromAppUser.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Onebrb.MVC.Data.Migrations
+{
+    public partial class RemovedNicknameFromAppUser : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Nickname",
+                table: "AspNetUsers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Nickname",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
--- Onebrb.MVC/Models/ApplicationUser.cs
+++ Onebrb.MVC/Models/ApplicationUser.cs
@@ -10,6 +10,5 @@ namespace Onebrb.MVC.Models
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public string Nickname { get; set; }
     }
 }
