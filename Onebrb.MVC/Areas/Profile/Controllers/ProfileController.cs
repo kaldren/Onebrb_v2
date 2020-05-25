0e045c6cd0ed9b3bc5d5ea66094c2bf7666751d7
kaldren <drenski666@gmail.com>
2020-05-25 22:37:36 +0300

    wip

---
+++ Onebrb.MVC/Areas/Profile/Controllers/ProfileController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Onebrb.MVC.Areas.Profile.Dtos;
+using Onebrb.MVC.Data;
+using Onebrb.MVC.Models;
+
+namespace Onebrb.MVC.Areas.Profile.Controllers
+{
+    [Area("Profile")]
+    [Route("[controller]/[action]")]
+    public class ProfileController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public ProfileController(ApplicationDbContext db, UserManager<ApplicationUser> userManager)
+        {
+            _db = db;
+            _userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("{username:alpha}")]
+        public new async Task<IActionResult> View(string username)
+        {
+            var user = await _db.Users.FirstOrDefaultAsync(x => x.UserName == username);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var dto = new ViewProfileDto
+            {
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                UserName = user.UserName
+            };
+
+            return View(dto);
+        }
+    }
+}
